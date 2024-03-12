7d53f7b1dec9e896cdb76e19e685fe65f122f89b
Yeuoly <admin@srmxy.cn>
2024-03-12 14:11:45 +0800

    feat: clean

--- build/build_amd64.sh
+++ build/build_amd64.sh
@@ -1,2 +1,4 @@
+rm -f internal/core/runner/python/python.so
+rm -f /tmp/sandbox-python/python.so
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o internal/core/runner/python/python.so -buildmode=c-shared -ldflags="-s -w" cmd/lib/python/main.go
 GOOS=linux GOARCH=amd64 go build -o main -ldflags="-s -w" cmd/server/main.go
