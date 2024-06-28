1044970f7d19e668c514b5345d38366dd79a718d
Yeuoly <admin@srmxy.cn>
2024-06-28 21:20:57 +0800

    fix: tests

--- tests/integration_tests/base.go
+++ tests/integration_tests/base.go
@@ -3,7 +3,6 @@ package integrationtests_test
 import "testing"
 
 func runMultipleTestings(t *testing.T, iterations int, testFunc func(*testing.T)) {
-	t.Parallel()
 	for i := 0; i < iterations; i++ {
 		testFunc(t)
 	}
