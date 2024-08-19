898c47aaccc7afefb5f8b43a6d202e47843d1a78
Yeuoly <admin@srmxy.cn>
2024-08-19 10:34:46 +0000

    cleanup

--- tests/integration_tests/init.go
+++ tests/integration_tests/init.go
@@ -9,7 +9,6 @@ import (
 func init() {
 	static.InitConfig("conf/config.yaml")
 
-	// Test case for sys_fork
 	err := python.PreparePythonDependenciesEnv()
 	if err != nil {
 		log.Panic("failed to initialize python dependencies sandbox: %v", err)
