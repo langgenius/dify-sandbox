df728d2817ce3a9710ddb95361357bf9fda4c145
wuhan1 <wuhan1@360.cn>
2024-09-10 18:14:27 +0800

    Modify python test case code

--- tests/integration_tests/python_requirements_test.go
+++ tests/integration_tests/python_requirements_test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/langgenius/dify-sandbox/internal/core/runner/types"
 	"github.com/langgenius/dify-sandbox/internal/service"
 	"github.com/langgenius/dify-sandbox/internal/static"
-	"github.com/langgenius/dify-sandbox/internal/utils/log"
 	"strings"
 	"testing"
 )
@@ -12,29 +11,9 @@ import (
 func TestPyModuleAutoImport(t *testing.T) {
 	code :=
 		`
-# declare main function
-
-def main(arg1: int, arg2: int) -> dict:
-        print(os.environ)
-        print(sys.path)
-        return {
-                "result": arg1 + arg2,
-        }
 import json
-from base64 import b64decode
-
-# decode and prepare input dict
-inputs_obj = json.loads(b64decode('eyJhcmcxIjogIjEyMSIsICJhcmcyIjogIjEyMSJ9').decode('utf-8'))
-
-# execute main function
-output_obj = main(**inputs_obj)
-
-# convert output to json and print
-output_json = json.dumps(output_obj, indent=4)
-result = f'''<<RESULT>>{output_json}<<RESULT>>'''
-print(result)
+print(json.dumps({"hello": "world"}))
 `
-	log.Info("code:%v\n", code)
 	configurations := static.GetDifySandboxGlobalConfigurations()
 	configurations.PythonModuleAutoImport = true
 	configurations.EnableNetwork = true
