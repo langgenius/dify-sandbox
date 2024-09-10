e0186cec21c6d861e61e8322805df112d015f3a7
Yeuoly <admin@srmxy.cn>
2024-09-10 14:49:03 +0000

    fix: missing libraries

--- internal/static/config_default_arm64.go
+++ internal/static/config_default_arm64.go
@@ -11,7 +11,6 @@ var DEFAULT_PYTHON_LIB_REQUIREMENTS = []string{
 	"/etc/resolv.conf",
 	"/run/systemd/resolve/stub-resolv.conf",
 	"/run/resolvconf/resolv.conf",
-	"/usr/lib/aarch64-linux-gnu/libssl.so.3",
-	"/usr/lib/aarch64-linux-gnu/libcrypto.so.3",
+	"/usr/lib/aarch64-linux-gnu",
 	"/etc/hosts",
 }
