b060a8b41c4da94cf9b17312c4eb7f2482fb5a5f
刘敏 <min.liu@tongdun.net>
2025-07-02 07:00:14 +0000

    optimize dockerfile to combine build stage and runtime stage

--- internal/static/config_default_amd64.go
+++ internal/static/config_default_amd64.go
@@ -3,8 +3,8 @@
 package static
 
 var DEFAULT_PYTHON_LIB_REQUIREMENTS = []string{
-	"/usr/local/lib/python3.10",
-	"/usr/lib/python3.10",
+	"/usr/local/lib/python3.12",
+	"/usr/lib/python3.12",
 	"/usr/lib/python3",
 	"/usr/lib/x86_64-linux-gnu",
 	"/etc/ssl/certs/ca-certificates.crt",
