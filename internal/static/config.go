cec0684e8311b8eca1019f9c2c26184a1aaf34ca
wangf <fan.wang@wiz.ai>
2025-11-27 14:28:00 +0800

    refactor: reduce cognitive complexity of InitConfig

--- internal/static/config.go
+++ internal/static/config.go
@@ -15,7 +15,24 @@ var difySandboxGlobalConfigurations types.DifySandboxGlobalConfigurations
 func InitConfig(path string) error {
 	difySandboxGlobalConfigurations = types.DifySandboxGlobalConfigurations{}
 
-	// read config file
+	if err := loadConfigFromFile(path); err != nil {
+		return err
+	}
+
+	overrideAppConfig()
+	overrideWorkerSettings()
+	overridePythonConfig()
+	overrideNodeConfig()
+
+	if err := overrideSyscalls(); err != nil {
+		return err
+	}
+
+	handleNetworkConfig()
+	return nil
+}
+
+func loadConfigFromFile(path string) error {
 	configFile, err := os.Open(path)
 	if err != nil {
 		return err
@@ -23,138 +40,144 @@ func InitConfig(path string) error {
 
 	defer configFile.Close()
 
-	// parse config file
 	decoder := yaml.NewDecoder(configFile)
-	err = decoder.Decode(&difySandboxGlobalConfigurations)
-	if err != nil {
-		return err
+	return decoder.Decode(&difySandboxGlobalConfigurations)
+}
+
+func overrideAppConfig() {
+	if debugStr := os.Getenv("DEBUG"); debugStr != "" {
+		if debug, err := strconv.ParseBool(debugStr); err == nil {
+			difySandboxGlobalConfigurations.App.Debug = debug
+		}
 	}
 
-	debug, err := strconv.ParseBool(os.Getenv("DEBUG"))
-	if err == nil {
-		difySandboxGlobalConfigurations.App.Debug = debug
+	if port := readIntEnv("SANDBOX_PORT"); port != nil {
+		difySandboxGlobalConfigurations.App.Port = *port
 	}
 
-	max_workers := os.Getenv("MAX_WORKERS")
-	if max_workers != "" {
-		difySandboxGlobalConfigurations.MaxWorkers, _ = strconv.Atoi(max_workers)
+	if apiKey := os.Getenv("API_KEY"); apiKey != "" {
+		difySandboxGlobalConfigurations.App.Key = apiKey
+	}
+}
+
+func overrideWorkerSettings() {
+	if maxWorkers := readIntEnv("MAX_WORKERS"); maxWorkers != nil {
+		difySandboxGlobalConfigurations.MaxWorkers = *maxWorkers
 	}
 
-	max_requests := os.Getenv("MAX_REQUESTS")
-	if max_requests != "" {
-		difySandboxGlobalConfigurations.MaxRequests, _ = strconv.Atoi(max_requests)
+	if maxRequests := readIntEnv("MAX_REQUESTS"); maxRequests != nil {
+		difySandboxGlobalConfigurations.MaxRequests = *maxRequests
 	}
 
-	port := os.Getenv("SANDBOX_PORT")
-	if port != "" {
-		difySandboxGlobalConfigurations.App.Port, _ = strconv.Atoi(port)
+	if timeout := readIntEnv("WORKER_TIMEOUT"); timeout != nil {
+		difySandboxGlobalConfigurations.WorkerTimeout = *timeout
 	}
 
-	timeout := os.Getenv("WORKER_TIMEOUT")
-	if timeout != "" {
-		difySandboxGlobalConfigurations.WorkerTimeout, _ = strconv.Atoi(timeout)
+	if preload := os.Getenv("ENABLE_PRELOAD"); preload != "" {
+		difySandboxGlobalConfigurations.EnablePreload, _ = strconv.ParseBool(preload)
 	}
+}
 
-	api_key := os.Getenv("API_KEY")
-	if api_key != "" {
-		difySandboxGlobalConfigurations.App.Key = api_key
-	}
-
-	python_path := os.Getenv("PYTHON_PATH")
-	if python_path != "" {
-		difySandboxGlobalConfigurations.PythonPath = python_path
+func overridePythonConfig() {
+	if pythonPath := os.Getenv("PYTHON_PATH"); pythonPath != "" {
+		difySandboxGlobalConfigurations.PythonPath = pythonPath
 	}
 
 	if difySandboxGlobalConfigurations.PythonPath == "" {
 		difySandboxGlobalConfigurations.PythonPath = "/usr/local/bin/python3"
 	}
 
-	python_lib_path := os.Getenv("PYTHON_LIB_PATH")
-	if python_lib_path != "" {
-		difySandboxGlobalConfigurations.PythonLibPaths = strings.Split(python_lib_path, ",")
+	if pythonLibPath := os.Getenv("PYTHON_LIB_PATH"); pythonLibPath != "" {
+		difySandboxGlobalConfigurations.PythonLibPaths = strings.Split(pythonLibPath, ",")
 	}
 
 	if len(difySandboxGlobalConfigurations.PythonLibPaths) == 0 {
 		difySandboxGlobalConfigurations.PythonLibPaths = DEFAULT_PYTHON_LIB_REQUIREMENTS
 	}
 
-	python_pip_mirror_url := os.Getenv("PIP_MIRROR_URL")
-	if python_pip_mirror_url != "" {
-		difySandboxGlobalConfigurations.PythonPipMirrorURL = python_pip_mirror_url
+	if pipMirrorURL := os.Getenv("PIP_MIRROR_URL"); pipMirrorURL != "" {
+		difySandboxGlobalConfigurations.PythonPipMirrorURL = pipMirrorURL
 	}
 
-	python_deps_update_interval := os.Getenv("PYTHON_DEPS_UPDATE_INTERVAL")
-	if python_deps_update_interval != "" {
-		difySandboxGlobalConfigurations.PythonDepsUpdateInterval = python_deps_update_interval
+	if depsInterval := os.Getenv("PYTHON_DEPS_UPDATE_INTERVAL"); depsInterval != "" {
+		difySandboxGlobalConfigurations.PythonDepsUpdateInterval = depsInterval
 	}
 
-	// if not set "PythonDepsUpdateInterval", update python dependencies every 30 minutes to keep the sandbox up-to-date
 	if difySandboxGlobalConfigurations.PythonDepsUpdateInterval == "" {
 		difySandboxGlobalConfigurations.PythonDepsUpdateInterval = "30m"
 	}
+}
 
-	nodejs_path := os.Getenv("NODEJS_PATH")
-	if nodejs_path != "" {
-		difySandboxGlobalConfigurations.NodejsPath = nodejs_path
+func overrideNodeConfig() {
+	if nodePath := os.Getenv("NODEJS_PATH"); nodePath != "" {
+		difySandboxGlobalConfigurations.NodejsPath = nodePath
 	}
 
 	if difySandboxGlobalConfigurations.NodejsPath == "" {
 		difySandboxGlobalConfigurations.NodejsPath = "/usr/local/bin/node"
 	}
+}
 
-	enable_network := os.Getenv("ENABLE_NETWORK")
-	if enable_network != "" {
-		difySandboxGlobalConfigurations.EnableNetwork, _ = strconv.ParseBool(enable_network)
+func overrideSyscalls() error {
+	allowedSyscalls := os.Getenv("ALLOWED_SYSCALLS")
+	if allowedSyscalls == "" {
+		return nil
 	}
 
-	enable_preload := os.Getenv("ENABLE_PRELOAD")
-	if enable_preload != "" {
-		difySandboxGlobalConfigurations.EnablePreload, _ = strconv.ParseBool(enable_preload)
+	strs := strings.Split(allowedSyscalls, ",")
+	ary := make([]int, len(strs))
+	for i := range ary {
+		value, err := strconv.Atoi(strs[i])
+		if err != nil {
+			return err
+		}
+		ary[i] = value
 	}
 
-	allowed_syscalls := os.Getenv("ALLOWED_SYSCALLS")
-	if allowed_syscalls != "" {
-		strs := strings.Split(allowed_syscalls, ",")
-		ary := make([]int, len(strs))
-		for i := range ary {
-			ary[i], err = strconv.Atoi(strs[i])
-			if err != nil {
-				return err
-			}
-		}
-		difySandboxGlobalConfigurations.AllowedSyscalls = ary
-	}
-
-	if difySandboxGlobalConfigurations.EnableNetwork {
-		log.Info("network has been enabled")
-		socks5_proxy := os.Getenv("SOCKS5_PROXY")
-		if socks5_proxy != "" {
-			difySandboxGlobalConfigurations.Proxy.Socks5 = socks5_proxy
-		}
-
-		if difySandboxGlobalConfigurations.Proxy.Socks5 != "" {
-			log.Info("using socks5 proxy: %s", difySandboxGlobalConfigurations.Proxy.Socks5)
-		}
-
-		https_proxy := os.Getenv("HTTPS_PROXY")
-		if https_proxy != "" {
-			difySandboxGlobalConfigurations.Proxy.Https = https_proxy
-		}
-
-		if difySandboxGlobalConfigurations.Proxy.Https != "" {
-			log.Info("using https proxy: %s", difySandboxGlobalConfigurations.Proxy.Https)
-		}
-
-		http_proxy := os.Getenv("HTTP_PROXY")
-		if http_proxy != "" {
-			difySandboxGlobalConfigurations.Proxy.Http = http_proxy
-		}
-
-		if difySandboxGlobalConfigurations.Proxy.Http != "" {
-			log.Info("using http proxy: %s", difySandboxGlobalConfigurations.Proxy.Http)
-		}
-	}
+	difySandboxGlobalConfigurations.AllowedSyscalls = ary
 	return nil
+}
+
+func handleNetworkConfig() {
+	overrideNetworkFlags()
+
+	if !difySandboxGlobalConfigurations.EnableNetwork {
+		return
+	}
+
+	log.Info("network has been enabled")
+	overrideProxy("SOCKS5_PROXY", &difySandboxGlobalConfigurations.Proxy.Socks5, "socks5 proxy")
+	overrideProxy("HTTPS_PROXY", &difySandboxGlobalConfigurations.Proxy.Https, "https proxy")
+	overrideProxy("HTTP_PROXY", &difySandboxGlobalConfigurations.Proxy.Http, "http proxy")
+}
+
+func overrideNetworkFlags() {
+	if enableNetwork := os.Getenv("ENABLE_NETWORK"); enableNetwork != "" {
+		difySandboxGlobalConfigurations.EnableNetwork, _ = strconv.ParseBool(enableNetwork)
+	}
+}
+
+func overrideProxy(env string, target *string, logLabel string) {
+	if value := os.Getenv(env); value != "" {
+		*target = value
+	}
+
+	if *target != "" {
+		log.Info("using %s: %s", logLabel, *target)
+	}
+}
+
+func readIntEnv(key string) *int {
+	value := os.Getenv(key)
+	if value == "" {
+		return nil
+	}
+
+	number, err := strconv.Atoi(value)
+	if err != nil {
+		return nil
+	}
+	return &number
 }
 
 // avoid global modification, use value copy instead
